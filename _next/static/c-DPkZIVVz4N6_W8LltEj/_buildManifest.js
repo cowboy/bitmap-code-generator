94df8b735059afe16896608e83bbedafff5a84ac
cowboy <cowboy@users.noreply.github.com>
2021-10-02 21:40:05 +0000

    deploy: e443bb6cf3cf298828b5eb206e5a65afcb973b46

--- _next/static/c-DPkZIVVz4N6_W8LltEj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/aa587bfe47a265d666e6.css","static/chunks/pages/index-7bd0d017c566e4730fef.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
